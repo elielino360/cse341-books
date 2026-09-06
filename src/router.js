5cb7f591a4f25356dae91d4877b5fb572e1c4a80
elielino360 <appschoolbic@gmail.com>
2026-09-06 22:29:08 +0100

    get-book-by-id

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
-import {getBooksHandler } from './controllers/books.js';
+import {getBooksHandler,getBookByIdHandler } from './controllers/books.js';
 import express from 'express';  
 
 const router = express.Router();
 
 router.get('/books', getBooksHandler);  
+
+router.get('/books/:id', getBookByIdHandler);
 
 export default router;
