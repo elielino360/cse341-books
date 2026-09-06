84b94bdd05a707cb6fcd368459aeb84847b57d8c
elielino360 <appschoolbic@gmail.com>
2026-09-06 21:03:36 +0100

    Connected app to MongoDB

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-import {connectToDb} from './src/db/connect.js';
 import app from './app.js';
+import { connectToDb, getDb } from './src/db/connect.js';
 
 const PORT = process.env.PORT;
+
 if (!PORT) {
-    throw new Error ('PORT is not defined. Make sure your local npm scripts reference the .env file with --env-file=.env, or define PORT in your hosted environment settings');
-} 
+  throw new Error('PORT is not defined. Make sure your local npm scripts reference the .env file with --env-file=.env, or define PORT in your hosted environment settings.');
+}
 
 const startServer = async () => {
-    try {
-        await connectToDb();   
-        app.listen(PORT, () => {
-            console.log(`Server is Listening on port ${PORT}`);
-        })
-    } catch (error) {
-        console.error('Error connecting to the database:', error.message);
-        process.exit(1);
-    } 
+  try {
+    await connectToDb();
+    const books = await getDb().collection('books').find({}).toArray();
+    console.log('Book documents:', books);    
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+    process.exit(1);
+  }
 };
 
 await startServer();
